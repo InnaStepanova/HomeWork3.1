59f92fe60573af0f9df9bf0631144936dfddb390
Inna Stepanova <inna-panichkina@yandex.ru>
2020-10-16 15:25:17 +0300

    Initial Commit

---
+++ HomeWork3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeWork3.1
+//
+//  Created by Инна Степанова on 16.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeWork3.1/HomeWork3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork3_1App.swift
+//  HomeWork3.1
+//
+//  Created by Инна Степанова on 16.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
